d29abff151384ff80d1fbedbf705f1fc044bbc8b
Rino Toma <simple1423kosen@gmail.com>
2017-08-24 10:40:48 +0900

    碁盤の修正

--- marubatsu.rb
+++ marubatsu.rb
@@ -1,12 +1,17 @@
 #〇×ゲーム
 side = 3  #一辺の長さ
+turn = ["〇", "×"]  #先手，後手のマーク
 
 print "〇×ゲームを始めます\n\n"
 
 #碁盤の作成
-for i in 1..9 do
-  print " #{i} |"
-  if (i%3) == 0 then
+arr = [1, 2, 3,
+       4, 5, 6,
+       7, 8, 9]
+
+for i in 0..8 do
+  print " #{arr[i]} |"
+  if (arr[i]%3) == 0 then
     print "\n"
     for j in 1..3 do
         print "---+"
@@ -16,4 +21,11 @@ for i in 1..9 do
 end
 
 print "場所を入力してください＞"
-num = gets.to_i  #場所の入力
+input = gets.to_i  #場所の入力(数値)
+
+=begin
+#入力された数値とのマッチング
+num = 0
+while input != num do
+  if input == num then
+=end
